3a63aad019e6060aafcd706f51d5c95eda930f49
guanlei <guanlei1981@gmail.com>
2019-04-08 14:29:45 +0800

    add: https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/

--- src/main/java/longestsubstringwithoutrepeatingcharacters/Solution.java
+++ src/main/java/longestsubstringwithoutrepeatingcharacters/Solution.java
@@ -1,7 +1,39 @@
 package longestsubstringwithoutrepeatingcharacters;
 
 public class Solution {
-    public int lengthOfLongestSubstring() {
-        return 0;
+    public static int lengthOfLongestSubstring(String s) {
+        StringBuilder container = new StringBuilder();
+
+        int max = 0;
+
+        int first = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            first = container.indexOf(String.valueOf(s.charAt(i)));
+
+            if(first != -1 ) {
+                container.append(s.charAt(i));
+                container.delete(0, first + 1);
+            }else {
+                container.append(s.charAt(i));
+
+            }
+
+            if(container.length() > max) {
+                max = container.length();
+            }
+        }
+
+        if(max < container.length()) {
+            max = container.length();
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int max = lengthOfLongestSubstring("cdd");
+        System.out.println(max);
     }
 }
